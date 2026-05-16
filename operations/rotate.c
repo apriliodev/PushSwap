322893cc92451b362ef570d6c95baaac2eab52b9
Aprilio <nrisk8031@gmail.com>
2026-05-16 20:32:26 +0200

    bdecourt: Add instructions

---
+++ operations/rotate.c
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdecourt <bdecourt@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/16 20:29:17 by bdecourt          #+#    #+#             */
+/*   Updated: 2026/05/16 20:30:05 by bdecourt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*Ajout de tout les ra rb rra etc la, 
+faire les fonctions pour les reutiliser dans le projet
+Apres c'est qu'une idée*/
