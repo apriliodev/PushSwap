4341567bd947b8afab31112e9490a1715755a016
Aprilio <nrisk8031@gmail.com>
2026-05-17 18:11:04 +0200

    bdecourt: Add algorithms folder with base structures files

--- operations/swap.c
+++ operations/swap.c
@@ -6,45 +6,43 @@
 /*   By: bdecourt <bdecourt@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 20:20:23 by bdecourt          #+#    #+#             */
-/*   Updated: 2026/05/17 01:42:53 by bdecourt         ###   ########.fr       */
+/*   Updated: 2026/05/17 18:09:14 by bdecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
 #include "libft.h"
 #include "printf.h"
+#include "push_swap.h"
 
-static void swaping(t_stack *stack)
+static void	swaping(t_stack *stack)
 {
-    int tmp;
-    
-    if (stack == NULL || stack->next == NULL)
-        return ;
-    tmp = stack->content;
-    stack->content = stack->next->content;
-    stack->next->content = tmp;
-    /*
-    tmp = stack->index;
-    stack->index = stack->next->index;
-    stack->next->index = tmp;
-    */
+	int	tmp;
+
+	if (stack == NULL || stack->next == NULL)
+		return ;
+	tmp = stack->content;
+	stack->content = stack->next->content;
+	stack->next->content = tmp;
+	tmp = stack->index;
+	stack->index = stack->next->index;
+	stack->next->index = tmp;
 }
 
-void do_sa(t_stack **stack_a)
+void	do_sa(t_stack **stack_a)
 {
-    swaping(*stack_a);
-    ft_printf("%s", "sa\n");
+	swaping(*stack_a);
+	ft_printf("%s", "sa\n");
 }
 
-void do_sb(t_stack **stack_b)
+void	do_sb(t_stack **stack_b)
 {
-    swaping(*stack_b);
-    ft_printf("%s", "sb\n");
+	swaping(*stack_b);
+	ft_printf("%s", "sb\n");
 }
 
-void do_ss(t_stack **stack_a, t_stack **stack_b)
+void	do_ss(t_stack **stack_a, t_stack **stack_b)
 {
-    swaping(*stack_a);
-    swaping(*stack_b);
-    ft_printf("%s", "ss\n");
+	swaping(*stack_a);
+	swaping(*stack_b);
+	ft_printf("%s", "ss\n");
 }
